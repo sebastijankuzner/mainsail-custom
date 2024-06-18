7a8d5b79cb650f75d41dd22be022ddb37a83c36d
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-06-18 06:59:00 +0000

    Increase payload

--- packages/p2p/source/constants.ts
+++ packages/p2p/source/constants.ts
@@ -6,8 +6,8 @@ export const constants = {
 	MAX_DOWNLOAD_BLOCKS: 400, // maximum number of blocks we can download at once
 	MAX_DOWNLOAD_BLOCKS_JOBS: 10, // maximum number of blocks jobs
 
-	MAX_PAYLOAD_CLIENT: 5 * Constants.Units.MEGABYTE, // default maxPayload value on the WS socket client
-	MAX_PAYLOAD_SERVER: 3 * Constants.Units.MEGABYTE,
+	MAX_PAYLOAD_CLIENT: 50 * Constants.Units.MEGABYTE, // default maxPayload value on the WS socket client
+	MAX_PAYLOAD_SERVER: 30 * Constants.Units.MEGABYTE,
 
 	MAX_PEERS_GET_API_NODES: 100,
 	// default maxPayload value on the  WS socket server
